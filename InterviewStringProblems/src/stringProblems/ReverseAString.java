77dc9c052355f7d9c25529bc3fd0e5cf7f1d4ff4
harshraj <harshraj@localhost>
2023-03-13 13:32:36 +0530

    Reverse a string

--- InterviewStringProblems/src/stringProblems/ReverseAString.java
+++ InterviewStringProblems/src/stringProblems/ReverseAString.java
@@ -3,8 +3,19 @@ package stringProblems;
 public class ReverseAString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// Reverse a string using For loop
+		String s = "harsh raj" ;
+		String rev = "" ;
+		
+		for(int j = s.length()-1; j>=0; j--) {
+			rev = rev + s.charAt(j);			
+		}
+		System.out.println("reverse using For loop:"+rev);
+		
+		//Reverse a string using StringBuffer class
+		StringBuffer sf = new StringBuffer(s);
+		System.out.println("reverse using StringBuffer class:"+sf.reverse());
 	}
 
 }
