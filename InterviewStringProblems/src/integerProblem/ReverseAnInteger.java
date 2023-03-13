211576d8db383320a048d7130eef620adba68680
harshraj <harshraj@localhost>
2023-03-13 14:13:58 +0530

    reverse an integer

---
+++ InterviewStringProblems/src/integerProblem/ReverseAnInteger.java
@@ -0,0 +1,16 @@
+package integerProblem;
+
+public class ReverseAnInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 1234567 ;
+		int rev = 0 ;
+		while(num!=0) {
+			rev = (rev*10)+(num%10);
+			num = num/10;
+		}
+		System.out.println(rev);
+	}
+
+}
